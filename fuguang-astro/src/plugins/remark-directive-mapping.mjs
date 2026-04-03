141f77cff76defe4c8dca4267997cabc1c8616e0
merin <zhanglinjun1126@gmail.com>
2026-04-04 00:31:18 +0800

    :ref功能

--- fuguang-astro/src/plugins/remark-directive-mapping.mjs
+++ fuguang-astro/src/plugins/remark-directive-mapping.mjs
@@ -3,6 +3,15 @@ import { visit } from 'unist-util-visit';
 export default function remarkDirectiveMapping() {
   return (tree) => {
     visit(tree, (node) => {
+      //  Text Directive (:ref)
+      if (node.type === 'textDirective') {
+        const data = node.data || (node.data = {});
+        
+        // 2. 将 :ref指令 转换为虚拟的 <directive-ref> 标签
+        data.hName = `directive-${node.name}`;
+        data.hProperties = node.attributes || {};
+      }
+       // containerDirective (:::accent) 的处理代码
       if (
         node.type === 'textDirective' ||
         node.type === 'leafDirective' ||
