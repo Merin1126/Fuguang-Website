20009b5479ba969f7d941c97f57dbcf53b432e76
merin <zhanglinjun1126@gmail.com>
2026-04-03 18:05:01 +0800

    content collection

---
+++ fuguang-astro/src/content.config.ts
@@ -0,0 +1,16 @@
+import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blogCollection = defineCollection({
+    loader: glob({ pattern: '**/*.mdx', base: './src/content/blog' }),
+    schema: z.object({
+        title: z.string(),
+        tag: z.string().optional(),
+        date: z.union([z.string(), z.date()]).optional(),
+        mainHeading: z.string().optional(),
+    }),
+});
+
+export const collections = {
+    blog: blogCollection,
+};
